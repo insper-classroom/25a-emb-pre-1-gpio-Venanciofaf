d521c74b1b1239694825c7186b5b662192659fb5
Venanciofaf <venanciofaf@al.insper.edu.br>
2025-02-12 11:12:44 -0300

    Todos os exercícios commitados

--- exe2/main.c
+++ exe2/main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 const int BTN_PIN = 26;
-
+const int BTN_PIN7 = 7;
 int main() {
   stdio_init_all();
 
@@ -11,10 +11,20 @@ int main() {
   gpio_set_dir(BTN_PIN, GPIO_IN);
   gpio_pull_up(BTN_PIN);
 
+  gpio_init(BTN_PIN7);
+  gpio_set_dir(BTN_PIN7, GPIO_IN);
+  gpio_pull_up(BTN_PIN7);
+
+  
   while (true) {
     if (!gpio_get(BTN_PIN)) {
       printf("Botao 1\n");
       while (!gpio_get(BTN_PIN)) {
+      };
+    }
+    if (!gpio_get(BTN_PIN7)) {
+      printf("Botao 2\n");
+      while (!gpio_get(BTN_PIN7)) {
       };
     }
   }
--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,52 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_PIN_VERDE = 26;
+const int BTN_PIN_VERMELHO= 28;
+
+
+
+const int LED_PIN_VERMELHO = 4; 
+const int LED_PIN_VERDE = 6;
 int main() {
   stdio_init_all();
 
+  // Inicializando os Botões 
+
+  gpio_init(BTN_PIN_VERDE);
+  gpio_set_dir(BTN_PIN_VERDE, GPIO_IN);
+  gpio_pull_up(BTN_PIN_VERDE);
+
+  gpio_init(BTN_PIN_VERMELHO);
+  gpio_set_dir(BTN_PIN_VERMELHO, GPIO_IN);
+  gpio_pull_up(BTN_PIN_VERMELHO);
+
+  // Inicializando os Leds 
+
+  gpio_init(LED_PIN_VERDE);
+  gpio_set_dir(LED_PIN_VERDE, GPIO_OUT);
+
+  gpio_init(LED_PIN_VERMELHO);
+  gpio_set_dir(LED_PIN_VERMELHO , GPIO_OUT);
+
+  // Estados 
+  bool estado_led_verde = false;
+  bool estado_led_vermelho = false;
+  
   while (true) {
+
+    if (!gpio_get(BTN_PIN_VERMELHO)) {
+      estado_led_vermelho = !estado_led_vermelho;
+      gpio_put(LED_PIN_VERMELHO , estado_led_vermelho);
+      while (!gpio_get(BTN_PIN_VERMELHO)) {
+      };
+    }
+   
+    if (!gpio_get(BTN_PIN_VERDE)) {
+      estado_led_verde = !estado_led_verde;
+      gpio_put(LED_PIN_VERDE , estado_led_verde);
+      while (!gpio_get(BTN_PIN_VERDE)) {
+      };
+    }
   }
 }
